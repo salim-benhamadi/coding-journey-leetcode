9524773857e8e83521c7fcd59da81ee95f206b0f
Salim Benhamadi <salim.benhamadi@outlook.com>
2024-03-21 22:06:51 +0100

    solved problem 2, 3, and 88

---
+++ 2. Add Two Numbers/2. Add Two Numbers.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        output = ListNode(0)  
+        tmp = output
+        carry = 0  
+        while l1 is not None or l2 is not None or carry != 0:  
+            sum_val = carry
+            if l1 is not None:
+                sum_val += l1.val
+                l1 = l1.next
+            if l2 is not None:
+                sum_val += l2.val
+                l2 = l2.next
+            carry = sum_val // 10
+            tmp.next = ListNode(sum_val % 10)
+            tmp = tmp.next
+        return output.next 
---
+++ 88. Merge Sorted Array/88. Merge Sorted Array.py
@@ -0,0 +1,8 @@
+
+nums1 = [1,2,3,0,0,0]  
+nums2 = [2,5,6]   
+m = 3
+n = 3
+nums1 = nums1[:m]+ nums2
+nums1.sort()
+print(nums1)
