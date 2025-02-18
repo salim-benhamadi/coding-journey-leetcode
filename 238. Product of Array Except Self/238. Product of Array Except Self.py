924b808961906259f1c809f8b5f627bcdfe92581
Salim Benhamadi <salim.benhamadi@outlook.com>
2025-02-18 20:09:05 +0100

    238. Product of Array Except Self

---
+++ 238. Product of Array Except Self/238. Product of Array Except Self.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        array_length = len(nums)
+        running_product = 1
+        result = []
+        
+        # Calculate left products
+        for i in range(array_length):
+            result.append(running_product)
+            running_product *= nums[i]
+            
+        # Calculate right products and combine
+        running_product = 1
+        for i in range(array_length - 1, -1, -1):
+            result[i] *= running_product
+            running_product *= nums[i]
+            
+        return result
