b55247d0b18615b6609be19dc90a15c116000fc4
Salim Benhamadi <salim.benhamadi@outlook.com>
2025-02-22 10:57:14 +0100

    Improve Leetode 70. Climbing Stairs

--- 70. Climbing Stairs/70. Climbing Stairs.py
+++ 70. Climbing Stairs/70. Climbing Stairs.py
@@ -1,14 +1,18 @@
+
+# Methode 1 : Recursive Solution (Time Limit Exceeded)
 class Solution:
     def climbStairs(self, n: int) -> int:
-        # Recursive Solution (Time Limit Exceeded)
+        
         if n == 0: return 1
         if n < 0: return 0
         return self.climbStairs(n-1) + self.climbStairs(n-2)
         
-        # Time complexity: O(2n)
-        # Space complexity: O(n)
+# Time complexity: O(2n)
+# Space complexity: O(n)
 
-        # Dynamic Programming Solution
+# Methode 2 : Dynamic Programming Solution
+class Solution:
+    def climbStairs(self, n: int) -> int:
         memory = [0]*(n+1)
         for i in range(n+1):
             if i == 0 or i - 1 == 0: 
@@ -17,5 +21,5 @@ class Solution:
                 memory[i] = memory[i-1] + memory[i-2]
         return memory[n]
 
-        # Time complexity: O(n)
-        # Space complexity: O(n)
+# Time complexity: O(n)
+# Space complexity: O(n)
