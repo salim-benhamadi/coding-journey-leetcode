9caa9629bdd8c567efdcd8425fd89f2273b6ebd0
Salim Benhamadi <salim.benhamadi@outlook.com>
2025-03-22 23:58:29 +0100

    Solve Leetcode 136. Single Number

---
+++ 136. Single Number/136. Single Number.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result = 0
+
+        for num in nums:
+            result ^= num
+
+        return result
+
+# Time complexity: O(n)
+# Space complexity: O(1) 
