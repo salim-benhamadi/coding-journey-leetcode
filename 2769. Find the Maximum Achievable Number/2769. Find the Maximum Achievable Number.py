46064899ea351d49481657db61cefaf6b06ff2cb
Salim Benhamadi <salim.benhamadi@outlook.com>
2025-10-20 13:19:41 +0200

    Solve Leetcode 2769. Find the Maximum Achievable Number

---
+++ 2769. Find the Maximum Achievable Number/2769. Find the Maximum Achievable Number.py
@@ -0,0 +1,7 @@
+# Mathematical solution
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + 2 * t
+
+# Time Complexity: O(1), constant time calculation
+# Space Complexity: O(1), only using constant extra space
